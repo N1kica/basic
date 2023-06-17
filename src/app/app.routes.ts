10bf13d2989adfd5d22a089c447d389d9fae8cbc
Nikica <kooh.kooh2@gmail.com>
2023-06-17 19:36:22 +0200

    basic app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layout/layout.component').then((m) => m.LayoutComponent),
+    loadChildren: () =>
+      import('./layout/layout.routes').then((m) => m.layoutRoutes),
+  },
+];
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NavbarComponent } from './ui/navbar.component';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [NavbarComponent, RouterOutlet],
+  template: `
+    <app-navbar />
+    <router-outlet />
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LayoutComponent {}
