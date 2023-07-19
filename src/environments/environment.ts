0ac19b5d67b78330778b26c41c3a5acd59379206
N1kica <68340084+N1kica@users.noreply.github.com>
2023-07-19 15:58:33 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyCwjPLB-roHKR_HCLQDYf1bw2dGYFVe-eE",
-    authDomain: "test-6e84c.firebaseapp.com",
-    projectId: "test-6e84c",
-    storageBucket: "test-6e84c.appspot.com",
-    messagingSenderId: "235772611679",
-    appId: "1:235772611679:web:cabadea2c288c567341fea",
-    measurementId: "G-N8C6X2ENEK"
+
   }
 }
